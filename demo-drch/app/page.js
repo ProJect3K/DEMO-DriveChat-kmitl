f217349f24d854d246b0d8e78e1161c9c6e73b50
Varaporn Sukmuang <wrp.sukmuang@gmail.com>
2024-11-04 17:46:57 +0700

    tawan whattttt

--- demo-drch/app/page.js
+++ demo-drch/app/page.js
@@ -30,6 +30,12 @@ export default function Home() {
           const usersPart = message.split(": ")[1];
           const users = usersPart.split(", ");
           setActiveUsers(users);
+        } else if (message.startsWith("System: ROOM_CHANGE:")) {
+          const newRoom = message.split(":")[2];
+          setRoom(newRoom);
+          // Re-establish websocket connection for new room
+          socket.current.close();
+          joinChat(newRoom);
         } else {
           setMessages((prevMessages) => [...prevMessages, message]);
         }
@@ -56,43 +62,51 @@ export default function Home() {
     }
   };
 
+  const handleRoomChange = (newRoom) => {
+    setRoom(newRoom);
+    if (newRoom === 'duck_pond') {
+      // Re-establish websocket connection for duck pond
+      socket.current.close();
+      joinChat('duck_pond');
+    }
+  };
+
   return (
-    <div className="min-h-screen bg-[url('/images/ต้นไม้มมม1.png')] bg-cover bg-center bg-fixed ">
-      <div className="min-h-screen bg-white/70 " >
-        <div className="max-w-4xl mx-auto p-6 bg-white/90 rounded-xl shadow-xl">
-          {!isJoined ? (
-            <JoinChat
-              username={username}
-              setUsername={setUsername}
-              isCreatingRoom={isCreatingRoom}
-              setIsCreatingRoom={setIsCreatingRoom}
-              selectedType={selectedType}
-              setSelectedType={setSelectedType}
-              roomName={roomName}
-              setRoomName={setRoomName}
-              customRoom={customRoom}
-              setCustomRoom={setCustomRoom}
-              isLoading={isLoading}
-              setIsLoading={setIsLoading}
-              room={room}
-              setRoom={setRoom}
-              setRoomCapacity={setRoomCapacity}
-              joinChat={joinChat}
-            />
-          ) : (
-            <ChatRoom
-              room={room}
-              activeUsers={activeUsers}
-              roomCapacity={roomCapacity}
-              username={username}
-              messages={messages}
-              inputMessage={inputMessage}
-              setInputMessage={setInputMessage}
-              sendMessage={sendMessage}
-            />
-          )}
-        </div>
-      </div>
+    <div className="max-w-4xl mx-auto p-6">
+      {!isJoined ? (
+        <JoinChat
+          username={username}
+          setUsername={setUsername}
+          isCreatingRoom={isCreatingRoom}
+          setIsCreatingRoom={setIsCreatingRoom}
+          selectedType={selectedType}
+          setSelectedType={setSelectedType}
+          roomName={roomName}
+          setRoomName={setRoomName}
+          customRoom={customRoom}
+          setCustomRoom={setCustomRoom}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+          room={room}
+          setRoom={setRoom}
+          setRoomCapacity={setRoomCapacity}
+          joinChat={joinChat}
+        />
+      ) : (
+        <ChatRoom
+          room={room}
+          setRoom={handleRoomChange}
+          activeUsers={activeUsers}
+          roomCapacity={roomCapacity}
+          username={username}
+          messages={messages}
+          inputMessage={inputMessage}
+          setInputMessage={setInputMessage}
+          sendMessage={sendMessage}
+          nextStation="ped pong"
+          currentStatus="Driving"
+        />
+      )}
     </div>
   );
 }
